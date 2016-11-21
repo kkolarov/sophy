8ad738f935211bc9e02b48d9f678dd6f9c7c2be7
Kamen Kolarov <k.p.kolarov@gmail.com>
2016-11-21 11:40:54 +0200

    Added a context manager that implements the idea behind the chain of responsibility pattern.

--- src/oracle/helpers/book.js
+++ src/oracle/helpers/book.js
@@ -13,39 +13,114 @@ const extractor = new EntityExtractor({
   day: true
 });
 
+const contextManager = (() => {
+  let handlers = [];
+
+  const checkDentist = () => {
+    return {
+      update: (context) => {
+        if (context.dentist) {
+          delete context.missing_dentist;
+
+          return false;
+        } else {
+          context.missing_dentist = true;
+
+          return true;
+        }
+      }
+    }
+  }
+
+  const checkReason = () => {
+    return {
+      update: (context) => {
+        if (context.reason) {
+          delete context.missing_reason;
+
+          return false;
+        } else {
+          context.missing_reason = true;
+
+          return true;
+        }
+      }
+    }
+  }
+
+  const checkDay = () => {
+    return {
+      update: (context) => {
+        if (context.day) {
+          delete context.missing_day;
+
+          return false;
+        } else {
+          context.missing_day = true;
+
+          return true;
+        }
+      }
+    }
+  }
+
+  const checkHour = () => {
+    return {
+      update: (context) => {
+        if (context.time) {
+          delete context.missing_time;
+
+          return false;
+        } else {
+          context.missing_time = true;
+
+          return true;
+        }
+      }
+    }
+  }
+
+  const validation = () => {
+    return {
+      update: (context) => {
+        return true;
+      }
+    }
+  }
+
+  const book = () => {
+    return {
+      update: (context) => {
+        context.done = true;
+
+        return true;
+      }
+    }
+  }
+
+  handlers = [checkDentist(), checkReason(), checkDay(), checkHour(), validation(), book()];
+
+  return {
+    update: (context) => {
+      for (let i = 0; i < handlers.length; ++i) {
+        if (handlers[i].update(context)) {
+          break;
+        }
+      }
+
+      return context;
+    }
+  };
+})();
+
+
 module.exports = ({context, entities}) => {
   return new Promise(function(resolve, reject) {
-    let newEntities = extractor.extract(entities);
-    let newContext = _.merge(context, newEntities);
+    const extractedEntities = extractor.extract(entities);
+    const mergedContext = _.merge(context, extractedEntities);
 
-    if (!newContext.dentist) {
-      delete newContext.missing_reason;
-      delete newContext.missing_day;
-      delete newContext.missing_time;
+    const updatedContext = contextManager.update(mergedContext);
 
-      newContext.missing_dentist = true;
-    } else if (!newContext.reason) {
-      delete newContext.missing_dentist;
-      delete newContext.missing_day;
-      delete newContext.missing_time;
-
-      newContext.missing_reason = true;
-    } else if (!newContext.day) {
-      delete newContext.missing_dentist;
-      delete newContext.missing_reason;
-      delete newContext.missing_time;
-
-      newContext.missing_day = true;
-    } else if (!newContext.time) {
-      delete newContext.missing_dentist;
-      delete newContext.missing_reason;
-      delete newContext.missing_day;
-
-      newContext.missing_time = true;
-    } else {
-      newContext.done = true;
-    }
-
-    return resolve(newContext);
+    return resolve(updatedContext);
   });
 };
