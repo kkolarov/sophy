796fbeead18fdee843bbb99f3ff4f29b8f766c7f
Kamen Kolarov <k.p.kolarov@gmail.com>
2016-12-13 19:00:16 +0200

    A text message before suggestions
    I have added the text "Първите свободни дни,
    в които можете да посетите кабинета." before showing up suggestions.

--- src/oracle/formulas/free-dates-gallery.js
+++ src/oracle/formulas/free-dates-gallery.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const DatesGallery = require('../../messenger/templates').DatesGallery;
+const TextReply = require('@fanatic/messenger').templates.TextReply;
 
 module.exports = ({ name, priority }) => {
   return {
@@ -14,12 +15,16 @@ module.exports = ({ name, priority }) => {
     consequence: function(R) {
       const context = this.prophecy.getContext();
 
+      const textReply = new TextReply(
+        this.prophecy.getRecipientId(),
+        "Първите свободни дни, в които можете да посетите кабинета."
+      );
       const suggestions = new DatesGallery(
         this.prophecy.getRecipientId(),
         context.suggestions
       );
 
-      this.replies = [suggestions];
+      this.replies = [textReply, suggestions];
 
       R.stop();
     }
