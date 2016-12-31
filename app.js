e9a93262df61e97f681baa1454bc14c9d4092384
Kamen Kolarov <k.p.kolarov@gmail.com>
2016-12-31 12:39:58 +0200

    Update(config) -> adding conversation logging configuration.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const config = require('config');
+const winston = require('winston');
 
 const Employee = require('./src/models/Employee');
 const Assistant = require('@fanatic/reservation').Assistant;
@@ -23,6 +24,14 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+winston.loggers.add('oracle', config.loggers.oracle);
+winston.loggers.add('assistant', config.loggers.assistant);
+winston.loggers.add('conversation', config.loggers.conversation);
+
+const oracleLogger = winston.loggers.get('oracle');
+const assistantLogger = winston.loggers.get('assistant');
+const conversationLogger = winston.loggers.get('conversation');
+
 const calendar = new GoogleCalendar(
   config.services.google.appId,
   config.services.google.appSecret,
@@ -31,8 +40,8 @@ const calendar = new GoogleCalendar(
 
 calendar.setToken(config.services.google.users.sophy);
 
-const conversationManager = new ConversationManager();
-const assistant = new Assistant(Employee, calendar);
+const conversationManager = new ConversationManager(conversationLogger);
+const assistant = new Assistant(Employee, calendar, assistantLogger);
 
 const formulas = require('./src/oracle/formulas')(conversationManager);
 const prophecyInterpreter = new ProphecyInterpreter(formulas);
@@ -40,7 +49,7 @@ const prophecyInterpreter = new ProphecyInterpreter(formulas);
 const messenger = new Messenger(prophecyInterpreter);
 
 const capabilities = require('./src/oracle/capabilities')(conversationManager, messenger, assistant);
-const oracle = new Oracle(capabilities, messenger, conversationManager);
+const oracle = new Oracle(capabilities, messenger, conversationManager, oracleLogger);
 
 const {
   fbRouter,
