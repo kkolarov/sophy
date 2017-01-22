e4889b4e3ccd96a773139d373424a074d4d22a79
Kamen Kolarov <k.p.kolarov@gmail.com>
2017-01-22 19:42:34 +0200

    Build(Oracle's capability existOneEmployee) -> check whether the page contains exactly one dentist before jumping to a dentist step.

---
+++ src/oracle/capabilities/exist-one-employee.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const Employee = require('../../models/Employee');
+
+const existOneEmployee = (manager) => {
+  return ({context, entities}) => {
+    return new Promise(function(resolve, reject) {
+      const userId = context.recipient.id;
+
+      manager.findConversationByUserId(userId)
+        .then(conversation => {
+          const page = conversation.metadata.page;
+
+          return Employee.findEmployeesByBusinessId(page._business, 1);
+        })
+        .then(employees => {
+          if (employees) {
+            const employee = employees[0];
+
+            context.dentist = {
+              name: employee.name,
+              pictureUrl: employee.pictureUrl,
+              calendarId: employee.calendarId,
+              workingTime: {
+                start: employee.workingTime.weekly.start,
+                end: employee.workingTime.weekly.end
+              },
+              address: employee._business.address
+            };
+          }
+
+          resolve(context);
+        })
+        .catch(err => reject(err));
+    });
+  }
+}
+
+module.exports = existOneEmployee;
